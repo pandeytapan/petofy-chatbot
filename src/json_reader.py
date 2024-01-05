eb787a551a96ee6040e51308bde17851f3e55f74
ANMOL2001A <anmolsharmajudoka@gmail.com>
2024-01-05 17:20:18 +0530

    created a dictonary for all json files

--- src/json_reader.py
+++ src/json_reader.py
@@ -1,22 +1,30 @@
 import json
+import os
 
-# json_file_path = "/home/anmol/Desktop/Petofy-chatbot/dataset/Consultant/Consultant.json"
-# with open(json_file_path, "r") as json_file:
-#     data = json.load(json_file)
-# print(data)
 def loading(file_path):
-    my_dict = {}
+    local_dict = {}
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
+        print(type(data))
         for i in data:
             prompt = i.get("Prompt")
             response = i.get("Response")
-            my_dict[prompt] = response
+            local_dict[prompt] = response
                 
+    return local_dict
+
+def load_json_files(base_directory):
+    my_dict = {}
+
+    if os.path.isdir(base_directory):
+        for root, dirs, files in os.walk(base_directory):
+            for file in files:
+                if file.endswith(".json"):
+                    file_path = os.path.join(root, file)
+                    local_dict = loading(file_path)
+                    my_dict.update(local_dict)
     return my_dict
 
-data_dict = loading('/home/anmol/Desktop/Petofy-chatbot/dataset/Consultant/Consultant.json')
-print(data_dict)
-
-
-
+base_directory_path = '/home/anmol/Desktop/Petofy-chatbot/dataset'
+result_dict = load_json_files(base_directory_path)
+print(result_dict)
